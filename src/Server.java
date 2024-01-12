0bbdd4d6f558945e3713d769ed6718c8c0986fac
thomas.wattebled2.etu <thomas.wattebled2.etu@univ-lille.fr>
2024-01-12 10:40:18 +0100

    creation project

---
+++ src/Server.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server {
+
+	
+	public static void main(String [] args) throws IOException {
+		ServerSocket server = new ServerSocket(2121);
+		System.out.println("lancement Server sur le port 2121 \n");
+		
+		System.out.println("oui \n");
+		
+	}
+}
