4a0a62c85e62c309cc1a72e480a13bdd086bf145
thomas.wattebled2.etu <thomas.wattebled2.etu@univ-lille.fr>
2024-01-15 11:12:47 +0100

    gerer jusqu'à la commande quit

--- src/Server.java
+++ src/Server.java
@@ -14,8 +14,8 @@ public class Server {
 		maplogin.put("thomas", "oui");
 		maplogin.put("valentin", "non");
 		
-		ServerSocket server = new ServerSocket(2124);
-		System.out.println("lancement Server sur le port 2124 \n");
+		ServerSocket server = new ServerSocket(2122);
+		System.out.println("lancement Server sur le port 2122 \n");
 		Socket s = new Socket();
 		s = server.accept();
 		System.out.println("Connexion avec une socket \n");
@@ -31,14 +31,42 @@ public class Server {
 
 		if (maplogin.containsKey(login)) {
 			str = "331 UserName Ok \r\n" ;
+			out = s.getOutputStream();
 			out.write(str.getBytes());
 			res = scanner.nextLine();
 			System.out.println(res);
 			String pass = res.substring(5);
 			if (maplogin.get(login).equals(pass)) {
 				str = "230 User log in\r\n" ;
+				out = s.getOutputStream();
 				out.write(str.getBytes());
-				res = scanner.nextLine();
+				res = scanner.nextLine();	
+				while(true) {
+					//int taille_res = res.length();
+					//res = res.substring(taille_res-4);
+					if (res.equals("SYST")) {
+						System.out.println("client deconnecte");
+						out.write(str.getBytes());
+						res = scanner.nextLine();
+					}
+					else if (res.equals("QUIT")) {
+						System.out.println("client deconnecte");
+						out = s.getOutputStream();
+						str = "221 deconnexion\r\n" ;
+						out.write(str.getBytes());
+						break;
+					}
+					else {
+						System.out.println(res);
+						out = s.getOutputStream();
+						str = "500 commande non reconnu\r\n" ;
+						out.write(str.getBytes());
+						res = scanner.nextLine();
+						//break ;
+					}
+				}
+				
+				
 			}
 			else {
 				str = "430 Identifiant ou mot de passe incorrect\r\n" ;
@@ -48,11 +76,6 @@ public class Server {
 		else {
 			str = "430 Identifiant ou mot de passe incorrect\r\n" ;
 			out.write(str.getBytes());
-		}
-		//res = scanner.nextLine();
-		while(!res.equals("quit")) {
-			res = scanner.nextLine();
-			
 		}
 	}
 }
